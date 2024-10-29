a190418686d6a162be3d5d360c1b0a973996a6b0
Samuel Smith <smit65785@gmail.com>
2024-10-29 15:27:31 -0400

    Changed EEPROM writing to split between EEPROMS for saftey

--- PSP-Main-Code.ino
+++ PSP-Main-Code.ino
@@ -22,44 +22,41 @@
 #define EEPROM1_ADDRESS 0x50 // First EEPROM
 #define EEPROM2_ADDRESS 0x51 // Second EEPROM
 #define EEPROM_SIZE 65536    // 64KB = 65536 bytes
-#define DATA_SIZE 4          // Number of analog inputs
+#define DATA_SIZE 2          // Number of analog inputs
 #define READ_INTERVAL 240    // 0.24 seconds (reduced to prevent drift from write delay)
 
-int currentAddress1 = 0; // Current address for EEPROM 1
-int currentAddress2 = 0; // Current address for EEPROM 2
+int currentAddress = 0; // Current address for both EEPROMs
 int logAddress = 65535;
 
 void setup() {
-    Wire.begin();
     Serial.begin(115200);
     Wire.begin(); // Start the I2C bus
     Wire.setClock(1000000); // Set I2C speed to 1 MHz
-
 }
 
 void loop() {
     // Read analog values from A0, A1, A2, A3
-    byte data[DATA_SIZE];
+    byte data1[DATA_SIZE];
+    byte data2[DATA_SIZE];
     for (int i = 0; i < DATA_SIZE; i++) {
-        data[i] = analogRead(A0 + i) / 4; // Scale to 0-255 for EEPROM
+        data1[i] = analogRead(A0 + i); //Log A0 and A1
+        data2[i] = analogRead(A2 + i); //Log A2 and A3
     }
 
     // Write data to EEPROM
-    if (currentAddress1 < EEPROM_SIZE) {
-        writeEEPROM(EEPROM1_ADDRESS, currentAddress1, data);
-        writeBackup(EEPROM1_ADDRESS, currentAddress1, logAddress);
-        currentAddress1++;
-    } else if (currentAddress2 < EEPROM_SIZE) {
-        writeEEPROM(EEPROM2_ADDRESS, currentAddress2, data);
-        writeBackup(EEPROM2_ADDRESS, currentAddress1, logAddress);
-        currentAddress2++;
-    } else {
+    if (currentAddress < EEPROM_SIZE) {
+        writeEEPROM(EEPROM1_ADDRESS, currentAddress, data1); //write A0 and A1 to EEPROM 1
+        writeEEPROM(EEPROM2_ADDRESS, currentAddress, data2); //write A2 and A3 to EEPROM 2
+
+        writeBackup(EEPROM1_ADDRESS, currentAddress, logAddress);
+        currentAddress++;
+      } else {
         // Both EEPROMs are full, stop writing
         Serial.println("Both EEPROMs are full. Stopping data collection.");
         while (true); // Stop further execution
     }
 
-    // Delay for 0.25 seconds
+    // Delay for 0.24 seconds
     delay(READ_INTERVAL);
 }
 
@@ -78,6 +75,7 @@ void writeBackup(int eepromAddress, int address, int logAddress) {
     Wire.beginTransmission(eepromAddress);
     Wire.write((int)(logAddress >> 8));   // Send high byte
     Wire.write((int)(logAddress & 0xFF)); // Send low byte
+    
     //Sending Last used address
     Wire.write((int)(address >> 8));   // Send high byte
     Wire.write((int)(address & 0xFF)); // Send low byte
